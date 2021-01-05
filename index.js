b67566b8c3364eb6bf00d3da4db7652137ff54bd
beenson <benson070707@yahoo.com.tw>
2021-01-05 16:04:43 +0800

    test

--- index.js
+++ index.js
@@ -10,44 +10,31 @@ var bot = linebot({
   channelAccessToken: 'ocCkvl0ipy7Fiw3tMA0nkeux0I82q1MM5vZIBA4PR3dEZKWoA9Yz1rCw9OnJAINKdtyjSiyoRE8L8SaFetDt9Cswr0wXYq52iuxFoGeSeHS+st0jDiomoYFFIHd2za3Fn7fd1qeSVqWMwX4zk0tEowdB04t89/1O/w1cDnyilFU='
 });
 
-var request = require("request");
-var cheerio = require("cheerio");
-var jp = function() {
-    request({url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm", method: "GET"}, function(error, response, body) {
-        if (error || !body) {
-            return
-        }else{
-            var $ = cheerio.load(body)
-            var target = $(".rate-content-sight.text-right.print_hide")
-            return target[15].children[0].data.trim()
-        // 爬完網頁後要做的事情
-        }
-    });
-};
+async function reply(event){
+    if (event.message.type = 'text') {
+      var msg = event.message.text;
+  
+      axios.get("https://rate.bot.com.tw/xrt?Lang=zh-TW").then((res) => { 
+              var $ = cheerio.load(res.data);
+              var target = $(".rate-content-sight.text-right.print_hide");
+              data = target[15].children[0].data.trim();
+  
+              //收到文字訊息時，直接把收到的訊息傳回去
+              event.reply(data).then(function(data) {
+                  // 傳送訊息成功時，可在此寫程式碼 
+                  console.log(data);
+              }).catch(function(error) {
+                  // 傳送訊息失敗時，可在此寫程式碼 
+                  console.log('錯誤產生，錯誤碼：'+error);
+              });})
+          .catch((error) => { console.error(error) })
+          .finally(() => {  /*不論失敗成功皆會執行*/  })
+      
+    }
+}
 
 //這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
-bot.on('message', function(event) {
-  if (event.message.type = 'text') {
-    var msg = event.message.text;
-
-    axios.get("http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm").then((res) => { 
-            var $ = cheerio.load(res.data);
-            var target = $(".rate-content-sight.text-right.print_hide");
-            data = target[15].children[0].data.trim();
-
-            //收到文字訊息時，直接把收到的訊息傳回去
-            event.reply(data).then(function(data) {
-                // 傳送訊息成功時，可在此寫程式碼 
-                console.log(data);
-            }).catch(function(error) {
-                // 傳送訊息失敗時，可在此寫程式碼 
-                console.log('錯誤產生，錯誤碼：'+error);
-            });})
-        .catch((error) => { console.error(error) })
-        .finally(() => {  /*不論失敗成功皆會執行*/  })
-    
-  }
-});
+bot.on('message', reply);
 
 const app = express();
 const linebotParser = bot.parser();
